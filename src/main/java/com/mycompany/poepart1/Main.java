4e61cc40ddddc90cb9554edaa778a9a24ede6381
Student <Student@LabVM2049939.ogaxydrls2euxawfnrt3am3kwe.jnbx.internal.cloudapp.net>
2026-09-17 21:15:29 +0200

    second commit

--- src/main/java/com/mycompany/poepart1/Main.java
+++ src/main/java/com/mycompany/poepart1/Main.java
@@ -35,44 +35,7 @@ public class Main {
 
         Login obj = new Login(firstName, lastName, username, password, cellPhoneNumber);
 
-        /*  boolean validUsername = false;                  //loop for the users username
-        while (!validUsername) {
-            System.out.println("Enter Username(must contain '_' and be <=5 chars)");
-            String username = scanner.nextLine();
-            if (userLogin.checkUserName(username)) {
-                userLogin.setusername(username);
-                System.out.println("Username was successfully captured");
-                validUsername = true;
-
-            } else {
-                System.out.println("Username is not correctly formatted; please ensure that your username contains an underscore and is not more than 5 characters");
-
-            }
-        }
-       boolean validPassword = false;        // loop for the users password
-        while (!validPassowrd) {
-            System.out.println("Enter password (minimun 8 chars, 1 uppercase, 1 digit, 1 special char): ");
-            String password = scanner.nextLine();
-            if (userLogin.checkPasswordComplexity(password)) {
-                userLogin.setPassword(password);
-                System.out.println("Password was successfully captured");
-                validPassword = true;
-            } else {
-                System.out.println("Password is incorrectly formmatted; plese ensure that your password contains 8 characters, a captial letter, a number, and a special character");
-            }
-        }
-        boolean validCell = false;
-        while (!validCell) {
-            System.out.println("Enter cell phone number (with international code, eg +27987562417):");
-            String cellPhone = scanner.nextLine();
-            if (userLogin.checkCellPhoneNumber()) {
-                userLogin.setcellPhoneNumber(cellPhone);
-                System.out.println("cell phone number is successfully captured");
-                validCell = true;
-            } else {
-                System.out.println("cell phone number is incorrectly formmated or does not contain an international code; please correct the number and try again");
-            }
-        } */
+        
         System.out.println("\nRegistration Status: " + obj.reisterUser(username, password));
 
         System.out.println("=================================================");
